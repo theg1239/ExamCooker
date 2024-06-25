7af14e6bfcf5beaa569df4c777adf38517a07a55
Alan-J-Bibins <alanjbibins@gmail.com>
2024-06-25 21:20:57 +0530

    feat: Made ForumPost page
    Used a dummy page.tsx file to showcase the ForumPost with some json data
    created

---
+++ app/forum/thread/ForumPost.tsx
@@ -0,0 +1,36 @@
+import CommentField from "@/app/components/CommentField";
+import CommentContainer from "@/app/components/CommentContainer";
+import { NumberOfComments } from "@/app/components/CommentContainer";
+import TagContainer from "../TagContainer";
+import { DislikeButton, LikeButton } from "@/app/components/Buttons";
+
+export default function ForumPost({ title, desc }: { title: string, desc: string }) {
+    let lorem: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl nunc mi ipsum faucibus vitae aliquet nec. Suspendisse potenti nullam ac tortor vitae purus. Ut sem nulla pharetra diam sit amet nisl suscipit. Nullam non nisi est sit amet facilisis. Augue ut lectus arcu bibendum at varius. Erat pellentesque adipiscing commodo elit. Sit amet aliquam id diam maecenas ultricies. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Enim diam vulputate ut pharetra sit amet aliquam. Nisi lacus sed viverra tellus in. Placerat in egestas erat imperdiet sed euismod nisi. Nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Nec nam aliquam sem et tortor consequat id porta. Massa eget egestas purus viverra accumsan in. Leo integer malesuada nunc vel risus.";
+
+    title = title === "" ? "Title of the Thread" : title;
+    desc = desc === "" ? lorem : desc;
+
+    return (
+        <div className="flex justify-center text-black">
+            <div className="bg-[#82BEE9] p-10 size-5/6">
+                <h2 className="font-extrabold">{title}</h2>
+                <hr className="border-0 h-px my-5 bg-black" />
+                <h6>{desc}</h6>
+                <br />
+                <div className="flex w-full justify-between">
+                    <TagContainer />
+                    <div className="hidden md:flex justify-center p-0.5 bg-white">
+                        <LikeButton />
+                        <DislikeButton />
+                    </div>
+                </div>
+
+                <br />
+                <NumberOfComments />
+                <CommentField />
+                <CommentContainer />
+            </div>
+        </div>
+    );
+}
+
