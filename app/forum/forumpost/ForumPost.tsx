f5bbf25d7284b61eb1c4177a21a7e0be953d1c21
Supratim Ghose <supratim7ghose@gmail.com>
2024-06-29 02:38:12 +0530

    fix: navbar margin

--- app/forum/forumpost/ForumPost.tsx
+++ app/forum/forumpost/ForumPost.tsx
@@ -11,15 +11,22 @@ import jsonData from "@/temp/testdata.json";
 const tags: Tag[] = jsonData.tags;
 const comments: Comment[] = jsonData.comments;
 
-export default function ForumPost({ title, desc }: { title: string, desc: string }) {
-    let lorem: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl nunc mi ipsum faucibus vitae aliquet nec. Suspendisse potenti nullam ac tortor vitae purus. Ut sem nulla pharetra diam sit amet nisl suscipit. Nullam non nisi est sit amet facilisis. Augue ut lectus arcu bibendum at varius. Erat pellentesque adipiscing commodo elit. Sit amet aliquam id diam maecenas ultricies. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Enim diam vulputate ut pharetra sit amet aliquam. Nisi lacus sed viverra tellus in. Placerat in egestas erat imperdiet sed euismod nisi. Nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Nec nam aliquam sem et tortor consequat id porta. Massa eget egestas purus viverra accumsan in. Leo integer malesuada nunc vel risus.";
+export default function ForumPost({
+    title,
+    desc,
+}: {
+    title: string;
+    desc: string;
+}) {
+    let lorem: string =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl nunc mi ipsum faucibus vitae aliquet nec. Suspendisse potenti nullam ac tortor vitae purus. Ut sem nulla pharetra diam sit amet nisl suscipit. Nullam non nisi est sit amet facilisis. Augue ut lectus arcu bibendum at varius. Erat pellentesque adipiscing commodo elit. Sit amet aliquam id diam maecenas ultricies. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Enim diam vulputate ut pharetra sit amet aliquam. Nisi lacus sed viverra tellus in. Placerat in egestas erat imperdiet sed euismod nisi. Nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Nec nam aliquam sem et tortor consequat id porta. Massa eget egestas purus viverra accumsan in. Leo integer malesuada nunc vel risus.";
 
     title = title === "" ? "Title of the Forum Post" : title;
     desc = desc === "" ? lorem : desc;
 
     return (
-        <div className="flex text-black">
-            <div className="bg-[#82BEE9] p-4 md:p-10 size-full h-screen overflow-auto">
+        <div className="flex text-black h-screen">
+            <div className="bg-[#82BEE9] p-4 md:p-10 overflow-auto">
                 <h2 className="font-extrabold">{title}</h2>
                 <hr className="border-0 h-px my-5 bg-black" />
                 <h6>{desc}</h6>
@@ -48,4 +55,3 @@ export default function ForumPost({ title, desc }: { title: string, desc: string
         </div>
     );
 }
-
