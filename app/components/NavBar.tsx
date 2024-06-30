139d9e044eeb58b2eaeacff1ce8ed6cd5984e048
Ojas Kulkarni <ojaskulkarni031@gmail.com>
2024-06-30 22:40:11 +0530

    fix: navbar overlapping searchbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,26 +1,11 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-const NavBar: React.FC = () => {
-  const [isNavOn, setIsNavOn] = useState(false);
-
-  const toggleNavbar = () => {
-    setIsNavOn(!isNavOn);
-    console.log(isNavOn ? "Nav Off" : "Nav On");
-  };
-
-  useEffect(() => {
-    if (isNavOn) {
-      document.body.classList.add('nav-on');
-    } else {
-      document.body.classList.remove('nav-on');
-    }
-  }, [isNavOn]);
-
+const NavBar: React.FC<{ isNavOn: boolean; toggleNavbar: () => void }> = ({ isNavOn, toggleNavbar }) => {
   return (
-    <nav className={`fixed top-0 left-0 flex flex-col justify-between items-center h-screen ${isNavOn ? "bg-[#5fc4e7]" : ""} text-white p-1 transition-all duration-300 ease-in-out`}>
+    <nav className={`absolute top-0 left-0 z-50 flex flex-col justify-between items-center h-screen ${isNavOn ? "bg-[#5fc4e7] lg:w-[5vw] md:w-[8vw] sm:w-[8vw]" : ""} text-white p-1 transition-all duration-300 ease-in-out`}>
       <div className="mt-4">
         <button onClick={toggleNavbar} className="opacity-100">
           <Image src="/assets/HamburgerIcon.svg" alt="Menu" width={30} height={30} className="transition-transform transform-gpu hover:scale-110 hover:-translate-y-1" />
@@ -42,17 +27,20 @@ const NavBar: React.FC = () => {
             <Image src="/assets/ForumIcon.svg" alt="Forum" width={24} height={25} className="transition-transform transform-gpu hover:scale-110 hover:-translate-y-1 hover:rotate-[-5deg]" />
           </div>
         </Link>
-        <Link href="/book" passHref>
+        {/* Dummy Path */}
+        <Link href="/resources" passHref>
           <div className={`m-2 ${isNavOn ? "block" : "hidden"}`}>
             <Image src="/assets/BookIcon.svg" alt="Book" width={24} height={25} className="transition-transform transform-gpu hover:scale-110 hover:-translate-y-1 hover:rotate-[-5deg]" />
           </div>
         </Link>
+        {/* Dummy Path */}
         <Link href="/favourites" passHref>
           <div className={`m-2 ${isNavOn ? "block" : "hidden"}`}>
             <Image src="/assets/NavFavouriteIcon.svg" alt="Favourites" width={24} height={25} className="transition-transform transform-gpu hover:scale-110 hover:-translate-y-1 hover:rotate-[-5deg]" />
           </div>
         </Link>
       </div>
+      {/* Dummy Path */}
       <div className="mb-4 bottom-5">
         <Link href="/logout" passHref>
           <div className={`m-2 ${isNavOn ? "block" : "hidden"}`}>
@@ -65,5 +53,3 @@ const NavBar: React.FC = () => {
 };
 
 export default NavBar;
-
-
