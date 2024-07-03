80fa82259b0079c9a78dccb23bd87ac21e8e8d46
Supratim Ghose <supratim7ghose@gmail.com>
2024-07-03 21:59:53 +0530

    fix:search bar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -31,21 +31,6 @@ const SearchBar: React.FC = () => {
         </div>
     );
 };
-  return (
-    <div className="relative flex items-center w-[75vw]">
-      <div className="absolute inset-0 bg-black translate-x-2 translate-y-2  pointer-events-none"></div>
-      <div className="relative flex items-center bg-white border border-black w-full px-4 ">
-        <FontAwesomeIcon icon={faSearch} color='grey' className='ml-4' />
-        <input
-          type="text"
-          className="px-4 py-2 w-full rounded-l-full focus:outline-none"
-          placeholder="Search"
-          value={query}
-          onChange={handleInputChange}
-        />
-      </div>
-    </div>
-  );
-}
+  
 
 export default SearchBar;
