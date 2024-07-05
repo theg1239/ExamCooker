c3831e06443522f4aaa2543a76ce5e5e944870f8
AnishaDhoot <anishadhoot26@gmail.com>
2024-07-05 12:43:20 +0530

    fix: before pull

--- app/components/uploadButtonPaper.tsx
+++ app/components/uploadButtonPaper.tsx
@@ -5,11 +5,19 @@ import Link from 'next/link';
 
 const UploadButtonPaper: React.FC = () => {
     return (
-        <Link
-            href="/create-papers"
-            className="bg-teal-400 border border-black border-2 text-black font-bold px-4 py-2  transition-colors duration-300 hover:bg-teal-500">
-            + New
-        </Link>
+        <div className="relative group">
+            <div className="absolute inset-0 bg-black">
+            </div>
+            <button type="submit" title="Login With Google " className="border-black border-2 relative px-4 py-2 text-lg bg-[#3BF4C7] text-black font-bold group-hover:-translate-x-1 group-hover:-translate-y-1 transition duration-150">
+                <Link
+                    href="/create-papers"
+                >
+                    + New
+                </Link>
+            </button>
+        </div>
+
+
     );
 };
 
