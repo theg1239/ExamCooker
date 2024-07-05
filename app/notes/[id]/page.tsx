7866ceaa20fae1f9bf433a0bae359b72c229a70d
mahendra785 <mahendrac482@gmail.com>
2024-07-05 13:15:32 +0530

    feat : added pdfviewer in notes and pastpapers

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import { PrismaClient } from '@prisma/client';
 import Link from 'next/link';
-import PdfViewer from '@/app/components/pdfviewer';
+import dynamic from 'next/dynamic';
+import PDFViewer from '@/app/components/pdfviewer';
 
-async function PdfViewerPage({params}: {params : {id : string}}) {
+async function PdfViewerPage({params }: {params : {id : string}}) {
   
   const prisma = new PrismaClient();
   const note = await prisma.note.findUnique({
@@ -16,27 +17,22 @@ async function PdfViewerPage({params}: {params : {id : string}}) {
     },
   });
 
-//note?.fileUrl
 
 return (
-  <div className="container mx-auto bg-[#C2E6EC] min-h-screen">
-      <div className="h-[10vh] w-full bg-[#C2E6EC] text-black text-center flex items-center justify-center">
-          header
-      </div>
-      <div className="grid grid-cols-4 lg:grid-cols-8 h-[90vh] gap-4">
-          <div className="col-span-4 lg:col-span-4 flex flex-col items-center justify-center mx-auto text-black pt-10">
+  <div className="container mx-auto">
+      <div className="flex flex-col lg:flex-row h-[90vh]">
+          <div className="w-full lg:w-1/2 flex flex-col items-center justify-center mx-auto text-black p-10">
               <h1 className="text-center">{note?.title}</h1><br />
-              <h2>Slot: Slot </h2><br />
-              <h2>Year: Year</h2><br />
+              <h2>Slot: A1</h2><br />
+              <h2>Year: 2024</h2><br />
           </div>
-          <div className="hidden lg:block w-[85vh] h-0 transform rotate-[90.13deg] origin-top-left border border-black opacity-80"></div>
-          <div className="col-span-4 lg:col-span-3 flex items-center justify-center">
-            {/* <p>{note?.fileUrl}</p> */}
-              <PdfViewer fileUrl={note?.fileUrl} />
+          <div className='flex flex-col w-full lg:w-1/2 items-center justify-center'>
+              <div className="h-[90vh] w-full pt-4 flex items-center justify-center lg:pl-24">
+                  <PDFViewer fileUrl={note?.fileUrl} />
+              </div>
           </div>
       </div>
   </div>
 );
 }
-
-export default PdfViewerPage;
+export default dynamic(() => Promise.resolve(PdfViewerPage), { ssr: false });
