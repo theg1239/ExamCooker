2d656fa678c214168e3eabdad2355eba26ed6a82
Alan-J-Bibins <alanjbibins@gmail.com>
2024-07-08 15:18:37 +0530

    fix: Removed black blur from New buttons of the past paper, notes and forum sections

--- app/components/uploadButtonPaper.tsx
+++ app/components/uploadButtonPaper.tsx
@@ -7,8 +7,8 @@ const UploadButtonPaper: React.FC = () => {
     return (
         <div className="relative group inline-flex items-center">
             <div className="absolute inset-0 bg-black dark:bg-[#3BF4C7]"></div>
-            <div className="absolute inset-0 blur-[75px] bg-black dark:lg:bg-none lg:dark:group-hover:bg-[#3BF4C7] transition dark:group-hover:duration-200 duration-1000" />
-            <button type="submit" title="Create New Note" className="border-black dark:border-[#D5D5D5] dark:group-hover:border-[#3BF4C7] dark:group-hover:text-[#3BF4C7] border-2 relative flex items-center px-4 py-2 text-lg bg-[#3BF4C7] dark:bg-[#0C1222] text-black dark:text-[#D5D5D5] font-bold group-hover:-translate-x-1 group-hover:-translate-y-1 transition duration-150">
+            <div className="dark:absolute dark:inset-0 dark:blur-[75px] dark:lg:bg-none lg:dark:group-hover:bg-[#3BF4C7] transition dark:group-hover:duration-200 duration-1000" />
+            <button type="submit" title="Upload New Past Paper" className="border-black dark:border-[#D5D5D5] dark:group-hover:border-[#3BF4C7] dark:group-hover:text-[#3BF4C7] border-2 relative flex items-center px-4 py-2 text-lg bg-[#3BF4C7] dark:bg-[#0C1222] text-black dark:text-[#D5D5D5] font-bold group-hover:-translate-x-1 group-hover:-translate-y-1 transition duration-150">
                 <Link href="/create-papers" className="flex items-center space-x-1">
                     <span className="text-xl">+</span>
                     <span className="text-lg">New</span>
