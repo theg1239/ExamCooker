4d83c8489ed6a6b23f70f38ca647683e7c4700d6
AnishaDhoot <anishadhoot26@gmail.com>
2024-07-12 04:43:24 +0530

    fix:ui changes for the home page

--- app/(portal)/home/home_header.tsx
+++ app/(portal)/home/home_header.tsx
@@ -4,11 +4,7 @@
 // function HomeHeader() {
 //   return (
 //     <header className="bg-[#CCF3FF] py-10 flex justify-center">
-//       <div className="container max-w-xl mx-auto px-4 sm:px-8 text-center">
-//         <h1 className="text-2xl mb-2 text-[#0070f3] sm:text-2xl">Cramming, Made Easy.</h1>
-//         <p className="text-lg mb-4 sm:text-xl">You&apos;ve got this! Even if this means a borderline psychotic level of caffeine consumption.</p>
-      
-//       </div>
+     
 //     </header>
 //   );
 // }
