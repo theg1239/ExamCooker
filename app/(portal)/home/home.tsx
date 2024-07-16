78c1cf8008864888bbc66a947df6a24f6d7391c8
Alan-J-Bibins <alanjbibins@gmail.com>
2024-07-16 18:29:25 +0530

    refactor: Removed extra fonts from home page

--- app/(portal)/home/home.tsx
+++ app/(portal)/home/home.tsx
@@ -23,14 +23,14 @@ const Home: React.FC = () => {
             <div className="flex-grow">
                 <div className="w-full px-8 mx-auto text-center justify-center mt-8">
                     <h1 className="text-3xl md:text-7xl mb-4">Welcome <GradientText><UserName /></GradientText></h1>
-                    <p className="text-md mb-4 sm:text-xl font-lato">You've got this! Even if 'this' means a borderline psychotic level of caffeine consumption.</p>
+                    <p className="text-md mb-4 sm:text-xl">You've got this! Even if 'this' means a borderline psychotic level of caffeine consumption.</p>
                 </div>
 
                 <div className="w-full px-4">
                     <div className="flex items-center justify-center text-2xl font-bold pt-6 mb-2">
                         <div className="relative flex items-center justify-between">
                             <div className="flex-grow border-t border-black dark:border-[#D5D5D5]"></div>
-                            <span className="font-montserrat">Recently viewed</span>
+                            <span>Recently viewed</span>
                             <Image src={ArrowRight} alt="arrow" className="h-6 w-6 ml-2" />
                             <div className="flex-grow border-t border-black dark:border-[#D5D5D5]"></div>
                         </div>
@@ -39,11 +39,11 @@ const Home: React.FC = () => {
                     <div className="flex justify-center h-[40vh] overflow-x-auto md:overflow-hidden">
                         <div className="w-full max-w-5xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-6 md:max-h-[20rem]">
                             {notes.slice(0, 3).map((note, index) => (
-                                <NotesCard 
-                                    key={note.id} 
-                                    index={index} 
-                                    note={note} 
-                                    className="max-h-[15rem] sm:max-h-[18rem] md:max-h-[20rem]" 
+                                <NotesCard
+                                    key={note.id}
+                                    index={index}
+                                    note={note}
+                                    className="max-h-[15rem] sm:max-h-[18rem] md:max-h-[20rem]"
                                 />
                             ))}
                         </div>
@@ -52,18 +52,18 @@ const Home: React.FC = () => {
 
                 <section className="mt-4 w-full px-4">
                     <div className="flex items-center justify-center text-2xl font-bold pt-6 mb-2">
-                        <span className="font-montserrat">Favourites </span>
+                        <span>Favourites </span>
                         <Image src={ArrowRight} alt="arrow" className="h-6 w-6 ml-2" />
                     </div>
 
                     <div className="flex justify-center h-[40vh] overflow-x-auto md:overflow-visible">
                         <div className="w-full max-w-5xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-6">
                             {notes.map((note, index) => (
-                                <NotesCard 
-                                    key={note.id} 
-                                    index={index} 
-                                    note={note} 
-                                    className="max-h-[15rem] sm:max-h-[18rem] md:max-h-[20rem]" 
+                                <NotesCard
+                                    key={note.id}
+                                    index={index}
+                                    note={note}
+                                    className="max-h-[15rem] sm:max-h-[18rem] md:max-h-[20rem]"
                                 />
                             ))}
                         </div>
