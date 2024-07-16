512e1c6d2e8dd1125018bc187dd1484e0669bfaa
Alan-J-Bibins <alanjbibins@gmail.com>
2024-07-16 17:34:34 +0530

    refactor: Made the Favourite buttons align properly

--- app/components/ResourceCard.tsx
+++ app/components/ResourceCard.tsx
@@ -24,7 +24,7 @@ function ResourceCard({ subject }: ResourceCardProps) {
     courseName = courseName ? courseName : "Subject Name";
 
     return (
-        <div className="flex-col justify-between w-full h-full p-4 bg-[#5FC4E7] border-b-2 border-b-[#5FC4E7] dark:border-b-[#3D414E] hover:border-b-2 hover:border-b-[#ffffff]  dark:hover:border-b-[#3BF4C7] dark:bg-[#0C1222] dark:hover:bg-[#ffffff]/10 transition-all duration-200 transform hover:scale-105 hover:shadow-lg">
+        <div className="flex flex-col justify-between w-full h-full p-4 bg-[#5FC4E7] border-b-2 border-b-[#5FC4E7] dark:border-b-[#3D414E] hover:border-b-2 hover:border-b-[#ffffff]  dark:hover:border-b-[#3BF4C7] dark:bg-[#0C1222] dark:hover:bg-[#ffffff]/10 transition-all duration-200 transform hover:scale-105 hover:shadow-lg">
             <div className='items-start'>
                 <Link href={`/resources/${subject.id}`} className="block w-full text-left">
                     <p>RESOURCES</p>
