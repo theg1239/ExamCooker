43999d5799a79243c3f7460aec0de2909d9bed64
Alan-J-Bibins <alanjbibins@gmail.com>
2024-07-18 16:57:51 +0530

    feat: more ;)

--- app/(portal)/home/display_username.tsx
+++ app/(portal)/home/display_username.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { useSession } from "next-auth/react";
-
+//smol easter eggss
 const UserName: React.FC = () => {
     const { data: session } = useSession();
     let name:string | null | undefined = session?.user?.name;
     if(name === "Supratim Ghose 22BIT0040"){
         name = "Suppu";
-    } else {
+    } else if(name === "Alan J Bibins 23BCE0598"){
+        name = "Aloo";
+    }
+    else {
         name = name?.split(' ',1)[0]
     }
     return <>
