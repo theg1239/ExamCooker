65fb668d38f6f4a0a822d7d4221f3412a54cc84e
Alan-J-Bibins <alanjbibins@gmail.com>
2024-07-19 23:18:08 +0530

    feat: Added new loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,24 +1,42 @@
-
+import LoadingSvg from "@/public/assets/Loader.svg"
+import Image from "next/image";
 export default function Loading() {
-  return (
-    <div
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        zIndex: 50,
-        color: 'white',
-      }}
-    >
-      <div className="flex justify-center">
-        <p>Loading...</p>
-      </div>
-    </div>
-  );
+
+    function getQuirkyLine() {
+        const lines = [
+            "Brewing up something amazing for you...",
+            "Grinding away at loading your content...",
+            "Loading up the next serving of coffee...",
+            "Adding a shot of speed to your load...",
+            "Avoiding work by loading things...",
+            "Brewing up a page just for you...",
+            "Procrastinating by making you wait for this load...",
+        ]
+
+
+        return lines[Math.floor(Math.random() * lines.length)];
+    }
+
+    return (
+        <div
+            style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100vw',
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                zIndex: 50,
+                color: 'white',
+            }}
+        >
+            <div className="flex flex-col justify-center items-center">
+                <Image src={LoadingSvg} alt="Loader" className="animate-spin size-1/3" />
+                <h6 className="p-5">{getQuirkyLine()}</h6>
+            </div>
+        </div>
+    );
 }
