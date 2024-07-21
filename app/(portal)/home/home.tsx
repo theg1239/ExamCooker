608d02acc58a6a1517cbd7b24306ab520b4a5bf6
Alan-J-Bibins <alanjbibins@gmail.com>
2024-07-22 01:29:11 +0530

    feat: Making the nothingviewed component responsive and uniform

--- app/(portal)/home/home.tsx
+++ app/(portal)/home/home.tsx
@@ -28,19 +28,25 @@ function getQuirkyLine() {
 function NothingViewedOrFav({ sectionName }: { sectionName: string }) {
     sectionName = sectionName === "Favourites" ? "favourited" : "viewed";
     return (
-        <div className="flex border rounded-lg w-fit p-10 border-[#82BEE9] dark:border-[#D5D5D5]">
-            <Image src={NothingViewedOrFavSvg} alt="Nothing Viewed Or Favourited" className="h-[100px] lg:h-[150px]" />
+        <div className="flex flex-col md:flex-row border rounded-lg w-fit md:w-2/3
+            p-2 md:p-10 border-[#82BEE9] dark:border-[#D5D5D5] 
+            justify-evenly items-center md:items-start">
+            <Image src={NothingViewedOrFavSvg} alt="Nothing Viewed Or Favourited" className="h-[100px] md:h-[150px] lg:h-[150px]" />
             <div className="flex flex-col gap-2 justify-center">
-                <h2>You have not {sectionName} anything</h2>
-                <p>Go to:</p>
-                <div className="flex *:px-1 p-2 bg-[#82BEE9] dark:bg-[#232530] w-fit py-1 rounded-lg *:rounded-lg ">
-                    <Link href={'/past_papers'} className="bg-inherit hover:bg-white/10">Past Papers</Link>
-                    <p>|</p>
-                    <Link href={'/notes'} className="bg-inherit hover:bg-white/10">Notes</Link>
-                    <p>|</p>
-                    <Link href={'/forum'} className="bg-inherit hover:bg-white/10">Forum</Link>
-                    <p>|</p>
-                    <Link href={'/resources'} className="bg-inherit hover:bg-white/10">Resources</Link>
+                <h3>You have not {sectionName} anything</h3>
+                <div className="space-y-2">
+                    <p>Go to:</p>
+                    <div className="flex *:px-1 p-2 bg-[#82BEE9] text-sm md:text-base 
+                        dark:bg-[#232530] rounded-lg *:rounded-md justify-between 
+                        md:justify-center w-full md:w-fit items-center">
+                        <Link href={'/past_papers'} className="bg-inherit hover:bg-white/10 ">Papers</Link>
+                        <p>|</p>
+                        <Link href={'/notes'} className="bg-inherit hover:bg-white/10">Notes</Link>
+                        <p>|</p>
+                        <Link href={'/forum'} className="bg-inherit hover:bg-white/10">Forum</Link>
+                        <p>|</p>
+                        <Link href={'/resources'} className="bg-inherit hover:bg-white/10">Resources</Link>
+                    </div>
                 </div>
             </div>
         </div>
