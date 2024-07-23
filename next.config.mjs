eae5250868f386b674d035017748d41ded6ddad9
RohitPhaniramSakamuri <rpsakamurii@gmail.com>
2024-07-24 01:43:03 +0530

    feat: microservice config and recieve.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['storage.googleapis.com', 'topperworld.in'],
+    },
+};
 
-export default nextConfig;
+export default nextConfig;
