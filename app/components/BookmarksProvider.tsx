7aec5cb194b821b0cb417ddbfdbf7aeb472467fa
RohitPhaniramSakamuri <rpsakamurii@gmail.com>
2024-07-24 13:02:12 +0530

    feat: useTransition hooks implemented

--- app/components/BookmarksProvider.tsx
+++ app/components/BookmarksProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { createContext, useState, useContext, useCallback } from 'react';
+import React, { createContext, useState, useContext, useCallback, useTransition } from 'react';
 import { Bookmark, toggleBookmarkAction } from '../actions/Favourites';
 
 type BookmarksContextType = {
@@ -21,6 +21,7 @@ export function useBookmarks() {
 
 export default function BookmarksProvider({ children, initialBookmarks }: { children: React.ReactNode, initialBookmarks: Bookmark[] }) {
     const [bookmarks, setBookmarks] = useState<Bookmark[]>(initialBookmarks);
+    const [pending, startTransition] = useTransition();
 
     const toggleBookmark = useCallback(async (bookmark: Bookmark, favourite: boolean) => {
         setBookmarks(prevBookmarks => {
@@ -32,32 +33,36 @@ export default function BookmarksProvider({ children, initialBookmarks }: { chil
             }
         });
 
-        try {
-            const result = await toggleBookmarkAction(bookmark, favourite);
+        startTransition(async () => {
+            try {
 
-            if (!result.success) {
+                const result = await toggleBookmarkAction(bookmark, favourite);
+
+                if (!result.success) {
+                    setBookmarks(prevBookmarks => {
+                        const index = prevBookmarks.findIndex(b => b.id === bookmark.id && b.type === bookmark.type);
+                        if (index > -1 && result.isBookmarked) {
+                            return [...prevBookmarks.slice(0, index), bookmark, ...prevBookmarks.slice(index + 1)];
+                        } else if (index === -1 && !result.isBookmarked) {
+                            return prevBookmarks.filter(b => b.id !== bookmark.id || b.type !== bookmark.type);
+                        }
+                        return prevBookmarks;
+                    });
+                    console.error('Failed to toggle bookmark:', result.error);
+                }
+
+            } catch (error) {
                 setBookmarks(prevBookmarks => {
                     const index = prevBookmarks.findIndex(b => b.id === bookmark.id && b.type === bookmark.type);
-                    if (index > -1 && result.isBookmarked) {
-                        return [...prevBookmarks.slice(0, index), bookmark, ...prevBookmarks.slice(index + 1)];
-                    } else if (index === -1 && !result.isBookmarked) {
-                        return prevBookmarks.filter(b => b.id !== bookmark.id || b.type !== bookmark.type);
+                    if (index > -1) {
+                        return prevBookmarks.filter((_, i) => i !== index);
+                    } else {
+                        return [...prevBookmarks, bookmark];
                     }
-                    return prevBookmarks;
                 });
-                console.error('Failed to toggle bookmark:', result.error);
+                console.error('Error toggling bookmark:', error);
             }
-        } catch (error) {
-            setBookmarks(prevBookmarks => {
-                const index = prevBookmarks.findIndex(b => b.id === bookmark.id && b.type === bookmark.type);
-                if (index > -1) {
-                    return prevBookmarks.filter((_, i) => i !== index);
-                } else {
-                    return [...prevBookmarks, bookmark];
-                }
-            });
-            console.error('Error toggling bookmark:', error);
-        }
+        })
     }, []);
 
     const isBookmarked = useCallback((id: string, type: Bookmark['type']) => {
--- app/components/PastPaperCard.tsx
+++ app/components/PastPaperCard.tsx
@@ -30,28 +30,27 @@ function PastPaperCard({ pastPaper, openInNewTab = false }: PastPaperCardProps) 
 
     return (
         <div className={`max-w-sm w-full h-full text-black dark:text-[#D5D5D5]`}>
-    <div className="hover:shadow-xl px-5 py-6 w-full text-center bg-[#5FC4E7] dark:bg-[#ffffff]/10 lg:dark:bg-[#0C1222] dark:border-b-[#3BF4C7] dark:lg:border-b-[#ffffff]/20 dark:border-[#ffffff]/20 border-2 border-[#5FC4E7] hover:border-b-[#ffffff] hover:border-b-2 dark:hover:border-b-[#3BF4C7]  dark:hover:bg-[#ffffff]/10 transition duration-200 transform hover:scale-105 max-w-96 cursor-pointer"
+            <div className="hover:shadow-xl px-5 py-6 w-full text-center bg-[#5FC4E7] dark:bg-[#ffffff]/10 lg:dark:bg-[#0C1222] dark:border-b-[#3BF4C7] dark:lg:border-b-[#ffffff]/20 dark:border-[#ffffff]/20 border-2 border-[#5FC4E7] hover:border-b-[#ffffff] hover:border-b-2 dark:hover:border-b-[#3BF4C7]  dark:hover:bg-[#ffffff]/10 transition duration-200 transform hover:scale-105 max-w-96 cursor-pointer"
                 onClick={() => router.push(`/past_papers/${pastPaper.id}`)}>
-        <div className="bg-[#d9d9d9] w-full h-44 relative overflow-hidden">
-         <img
-            className="w-full object-cover"
-             src={pastPaper.thumbNailUrl || "https://topperworld.in/media/2022/11/c-sc.png"} // migration to make thumbnail mandatory 
-             alt={removePdfExtension(pastPaper.title)}
-         />
-     </div>
-     <div className="flex justify-between items-center">
-        <div></div>
-        <div className="mb-2 w-full whitespace-nowrap overflow-hidden text-ellipsis text-lg">
-            {removePdfExtension(pastPaper.title)}
+                <div className="bg-[#d9d9d9] w-full h-44 relative overflow-hidden">
+                    <img
+                        className="w-full object-cover"
+                        src={pastPaper.thumbNailUrl || "https://topperworld.in/media/2022/11/c-sc.png"} // migration to make thumbnail mandatory 
+                        alt={removePdfExtension(pastPaper.title)}
+                    />
+                </div>
+                <div className="flex justify-between items-center">
+                    <div></div>
+                    <div className="mb-2 w-full whitespace-nowrap overflow-hidden text-ellipsis text-lg">
+                        {removePdfExtension(pastPaper.title)}
+                    </div>
+                    <button onClick={handleToggleFav} className="transition-colors duration-200">
+                        <FontAwesomeIcon icon={faHeart} color={isFav ? 'red' : 'lightgrey'} />
+                    </button>
+                </div>
+                <div {...(openInNewTab ? { target: "_blank", rel: "noopener noreferrer" } : {})}></div>
+            </div>
         </div>
-        <button onClick={handleToggleFav} className="transition-colors duration-200">
-             <FontAwesomeIcon icon={faHeart} color={isFav ? 'red' : 'lightgrey'} />
-         </button>
-     </div>
-         <div {...(openInNewTab ? { target: "_blank", rel: "noopener noreferrer" } : {})}>
-         </div>
- </div>
-</div>
     );
 }
 
