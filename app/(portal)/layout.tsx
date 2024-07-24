ea2bbfbde043bd12df3cf1b76f3ac0235480d929
Alan-J-Bibins <alanjbibins@gmail.com>
2024-07-25 02:54:48 +0530

    fix: Fixed position of Footer while loading

--- app/(portal)/layout.tsx
+++ app/(portal)/layout.tsx
@@ -19,7 +19,6 @@ export default async function Layout({
             <SessionProvider>
                 {children}
             </SessionProvider>
-            <HomeFooter />
         </ClientSide>
     );
 }
