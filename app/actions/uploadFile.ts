21e21a3c1de77aa75c38088a8a8ae2b608587039
kairavsheth <85606266+kairavsheth@users.noreply.github.com>
2024-07-25 18:20:38 +0530

    direct file upload

--- app/actions/uploadFile.ts
+++ app/actions/uploadFile.ts
@@ -15,12 +15,77 @@ async function findOrCreateTag(name: string) {
     return tag;
 }
 
-async function preInsert({tags, year, slot, formDatas}: {
+// async function preInsert({tags, year, slot, formDatas}: {
+//     tags: string[],
+//     year: string,
+//     slot: string,
+//     formDatas: FormData[],
+// }) {
+//     const session = await auth();
+//     if (!session || !session.user) {
+//         redirect("/landing");
+//     }
+//     const user = await prisma.user.findUnique({
+//         where: {email: session.user.email!},
+//     });
+//
+//     if (!user) {
+//         throw new Error(
+//             `User with ID ${session?.user?.email} does not exist`
+//         );
+//     }
+//
+//     const allTags = await Promise.all(tags.map(findOrCreateTag));
+//
+//     if (year) {
+//         const yearTag = await findOrCreateTag(year);
+//         allTags.push(yearTag);
+//     }
+//
+//     if (slot) {
+//         const slotTag = await findOrCreateTag(slot);
+//         allTags.push(slotTag);
+//     }
+//     const promises = formDatas.map(async (formData) => {
+//
+//         const response = await fetch(`${process.env.NEXT_PUBLIC_MICROSERVICE_URL}/process_pdf`, {
+//             method: "POST",
+//             body: formData,
+//         });
+//
+//         if (!response.ok) {
+//             console.log(response);
+//             throw new Error(`Failed to upload file ${formData.get("fileTitle")}`);
+//         }
+//
+//         return await response.json();
+//     });
+//
+//     const results = await Promise.all(promises) as {
+//         fileUrl: string,
+//         thumbnailUrl: string,
+//         filename: string,
+//         message: string
+//     }[];
+//
+//
+//     return {user, allTags, results};
+// }
+
+export default async function uploadFile({results, tags, year, slot, variant}: {
+    results: {
+        fileUrl: string,
+        thumbnailUrl: string,
+        filename: string,
+        message: string
+    }[],
     tags: string[],
     year: string,
     slot: string,
-    formDatas: FormData[],
+    variant: "Notes" | "Past Papers"
 }) {
+
+    // const {allTags, user, results} = await preInsert({tags, year, slot, formDatas});
     const session = await auth();
     if (!session || !session.user) {
         redirect("/landing");
@@ -46,41 +111,6 @@ async function preInsert({tags, year, slot, formDatas}: {
         const slotTag = await findOrCreateTag(slot);
         allTags.push(slotTag);
     }
-    const promises = formDatas.map(async (formData) => {
-
-        const response = await fetch(`${process.env.NEXT_PUBLIC_MICROSERVICE_URL}/process_pdf`, {
-            method: "POST",
-            body: formData,
-        });
-
-        if (!response.ok) {
-            console.log(response);
-            throw new Error(`Failed to upload file ${formData.get("fileTitle")}`);
-        }
-
-        return await response.json();
-    });
-
-    const results = await Promise.all(promises) as {
-        fileUrl: string,
-        thumbnailUrl: string,
-        filename: string,
-        message: string
-    }[];
-
-
-    return {user, allTags, results};
-}
-
-export default async function uploadFile({formDatas, tags, year, slot, variant}: {
-    formDatas: FormData[],
-    tags: string[],
-    year: string,
-    slot: string,
-    variant: "Notes" | "Past Papers"
-}) {
-
-    const {allTags, user, results} = await preInsert({tags, year, slot, formDatas});
 
     const errors = results.filter(result => result.message !== "processed successfully");
 
