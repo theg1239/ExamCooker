b2a4efcba77617a2c9bb7bdb86ae68fa2f43cdf3
Nitesh Kakkar <121416598+Nitesh-04@users.noreply.github.com>
2024-10-05 22:13:40 +0530

    Update page.tsx

--- app/@protected_routes/page.tsx
+++ app/@protected_routes/page.tsx
@@ -4,5 +4,5 @@ import Home from "@/app/@protected_routes/home/home";
 export default async function Page() {
     const session = await auth();
 
-    return <>{session ? <Home /> : <div>Hello World!✌️</div>}</>;
+    return <>{session ? <Home /> : <div>You need to be logged in to see this.🚫</div>}</>;
 }
