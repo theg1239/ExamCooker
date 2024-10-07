7337bf475c4d505a964b3f27359355178d6aa91e
Alan-J-Bibins <alanjbibins@gmail.com>
2024-10-07 14:48:06 +0530

    refactor: removed unnecessary code

--- app/components/ShareLink.tsx
+++ app/components/ShareLink.tsx
@@ -3,20 +3,6 @@ import { faShareNodes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react'
 
-
-const IndicatorOfSuccess = () => {
-    return (
-        <div className="group relative">
-            <div
-                className="absolute left-full ml-2 top-1/2 -translate-y-1/2 px-3 py-2 bg-gradient-to-r from-[#5fc4e7] to-[#4db3d6] dark:from-[#3BF4C7] dark:to-[#2ad3a7] text-white dark:text-[#232530] rounded-md text-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out z-50 whitespace-nowrap shadow-lg backdrop-blur-sm backdrop-filter max-w-xs break-words">
-                <span className="font-medium">Copied Link!</span>
-                <div
-                    className="absolute w-0 h-0 border-t-[6px] border-b-[6px] border-r-[6px] border-transparent border-r-[#5fc4e7] dark:border-r-[#3BF4C7] -left-[6px] top-1/2 -translate-y-1/2 transform transition-transform duration-300 ease-in-out group-hover:scale-110"></div>
-            </div>
-        </div>
-    )
-}
-
 const ShareLink = ({fileType} : {fileType:string}) => {
 
     const [isVisible, setisVisible] = useState(false);
