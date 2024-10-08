6e4b44f216ef1f3f2bf8cc0dc58ae7aff34a08b8
Alan-J-Bibins <alanjbibins@gmail.com>
2024-10-08 21:35:58 +0530

    refactor: Made the position of success indicator uniform

--- app/components/ShareLink.tsx
+++ app/components/ShareLink.tsx
@@ -3,7 +3,7 @@ import { faShareNodes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react'
 
-const ShareLink = ({fileType} : {fileType:string}) => {
+const ShareLink = ({ fileType }: { fileType: string }) => {
 
     const [isVisible, setisVisible] = useState(false);
 
@@ -32,33 +32,20 @@ const ShareLink = ({fileType} : {fileType:string}) => {
 
     return (
         <button onClick={handleClick}>
-            <div className="group relative block md:hidden">
+            <div className="group relative">
                 <div className={`
                 px-3 py-2 absolute right-0 bottom-0 -translate-y-full
-                text-white dark:text-[#232530] text-sm font-medium 
+                text-white dark:text-[#232530] text-sm font-medium break-normal 
 
                 bg-gradient-to-b from-[#5fc4e7] to-[#4db3d6] dark:from-[#3BF4C7] dark:to-[#2ad3a7] shadow-lg rounded-md
                 transition-all duration-300 ease-in-out
-                ${isVisible ? 'opacity-100 -translate-y-0' : 'opacity-0 translate-y-1'}
+                ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-1'}
                 `}
                 >
                     Copied!
                 </div>
             </div>
             <FontAwesomeIcon icon={faShareNodes} />
-            <div className="group relative hidden md:block">
-                <div className={`
-                px-3 py-2 absolute right-0 translate-x-1/2 
-                text-white dark:text-[#232530] text-sm font-medium 
-
-                bg-gradient-to-b from-[#5fc4e7] to-[#4db3d6] dark:from-[#3BF4C7] dark:to-[#2ad3a7] shadow-lg rounded-md
-                transition-all duration-300 ease-in-out
-                ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-1'}
-                `}
-                >
-                    Copied!
-                </div>
-            </div>
         </button>
     );
 }
