fbe5993f9723d13518cca249d3d3a4b5dac3c88b
Alan-J-Bibins <alanjbibins@gmail.com>
2024-10-11 19:42:30 +0530

    fix: added borders and fixed button position of X

--- components/ui/SocialMediaToast.tsx
+++ components/ui/SocialMediaToast.tsx
@@ -7,51 +7,51 @@ import GiftBoxIcon from '@/public/assets/GiftBox.svg';
 
 const SocialMediaFollowToast = () => {
   const [showToast, setShowToast] = useState(false);
-
-  useEffect(() => {
-    const checkAndShowToast = () => {
-      const storedData = localStorage.getItem('socialMediaToastData');
-      const currentTime = new Date().getTime();
-
-      if (!storedData) {
-        showToastNotification();
-        return;
-      }
-
-      const { timestamp, hasSeenToast } = JSON.parse(storedData);
-      const expirationTime = 12 * 60 * 60 * 1000;
-
-      if (!hasSeenToast || currentTime - timestamp > expirationTime) {
-        showToastNotification();
-      }
-    };
-
-    const showToastNotification = () => {
-      setShowToast(true);
-      localStorage.setItem('socialMediaToastData', JSON.stringify({
-        hasSeenToast: true,
-        timestamp: new Date().getTime()
-      }));
-
-      setTimeout(() => {
-        setShowToast(false);
-      }, 20000 );
-    };
-
-    checkAndShowToast();
-  }, []);
-
-  if (!showToast) return null;
+  //
+  // useEffect(() => {
+  //   const checkAndShowToast = () => {
+  //     const storedData = localStorage.getItem('socialMediaToastData');
+  //     const currentTime = new Date().getTime();
+  //
+  //     if (!storedData) {
+  //       showToastNotification();
+  //       return;
+  //     }
+  //
+  //     const { timestamp, hasSeenToast } = JSON.parse(storedData);
+  //     const expirationTime = 12 * 60 * 60 * 1000;
+  //
+  //     if (!hasSeenToast || currentTime - timestamp > expirationTime) {
+  //       showToastNotification();
+  //     }
+  //   };
+  //
+  //   const showToastNotification = () => {
+  //     setShowToast(true);
+  //     localStorage.setItem('socialMediaToastData', JSON.stringify({
+  //       hasSeenToast: true,
+  //       timestamp: new Date().getTime()
+  //     }));
+  //
+  //     setTimeout(() => {
+  //       setShowToast(false);
+  //     }, 20000 );
+  //   };
+  //
+  //   checkAndShowToast();
+  // }, []);
+  //
+  // if (!showToast) return null;
 
   return (
-    <div className="fixed bottom-0 right-4 z-50 max-w-sm px-4 bg-[#C2E6EC] rounded-lg shadow-xl dark:bg-[#0C1222]">
+    <div className="fixed bottom-0 right-4 z-50 max-w-sm px-4 bg-[#C2E6EC] rounded-lg shadow-xl dark:bg-[#0C1222] border border-[#2699E9] dark:border-[#3BF4C7]">
       <div className="flex justify-end">
         <button
           type="button"
           onClick={() => setShowToast(false)}
           className="text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
         >
-          <X size={24} />
+          <X size={24} className='mt-2'/>
         </button>
       </div>
       <div className="flex flex-col items-center">
@@ -60,7 +60,7 @@ const SocialMediaFollowToast = () => {
           alt="Gift Box"
           width={240}
           height={272}
-          className="mb-0 translate-y-5"
+          className="mb-0 translate-y-5 size-1/2"
           
         />
         <h6 className="text-lg font-bold text-center text-gray-900 dark:text-white mb-2">
@@ -74,7 +74,7 @@ const SocialMediaFollowToast = () => {
           target="_blank"
           rel="noopener noreferrer"
           className="inline-flex items-center justify-center w-full px-4 py-2 text-lg font-bold text-black border border-[#5FC4E7] bg-gray 
-          dark:border-teal-400 rounded-md hover:bg-black/20 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:text-teal-400 dark:border-[#82BEE9] 
+          dark:border-[#3BF4C7] rounded-md hover:bg-black/20 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:text-[#3BF4C7] 
           dark:hover:bg-white/20 dark:focus:ring-black-400 mb-4">
           Follow on Instagram
         </a>
