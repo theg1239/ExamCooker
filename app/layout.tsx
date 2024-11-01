099ab5393e3bf902fd643a0f580371c1f95f7e6d
Supratim Ghose <128067781+Supratim69@users.noreply.github.com>
2024-11-02 01:54:05 +0530

    Revert "Quiz section"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,35 @@
 import type React from "react";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-import { auth } from "@/app/auth";
+import {auth} from "@/app/auth";
 import "@/app/globals.css";
 import SocialMediaFollowToast from "@/components/ui/SocialMediaToast";
-import { redirect } from "next/navigation";
+import {redirect} from "next/navigation";
 
 export const metadata = {
     title: {
-        template: "%s | ExamCooker",
-        default: "ExamCooker - ACM-VIT",
+        template: '%s | ExamCooker',
+        default: 'ExamCooker - ACM-VIT',
     },
-    description: "Cram up for your exams with ExamCooker!! ",
-    keywords: [
-        "vit",
-        "previous year question papers",
-        "pdf",
-        "notes",
-        "question papers",
-        "exam",
-        "examcooker",
-        "acm",
-        "vit acm",
-        "vit acm examcooker",
-    ],
+    description: "Cram up for your exams with ExamCooker!",
+    keywords: ['vit', 'previous year question papers', 'pdf', 'notes', 'question papers', 'exam', 'examcooker', 'acm', 'vit acm', 'vit acm examcooker'],
     metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL!),
     openGraph: {
         images: [
-            { url: `${process.env.NEXT_PUBLIC_BASE_URL!}/opengraph-image.png` },
-        ],
-    },
+            {url: `${process.env.NEXT_PUBLIC_BASE_URL!}/opengraph-image.png`}
+        ]
+    }
 };
 const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export default async function RootLayout({
-    protected_routes,
-    unprotected_routes,
+    protected_routes, unprotected_routes
 }: {
     protected_routes: React.ReactNode;
     unprotected_routes: React.ReactNode;
 }) {
     const session = await auth();
+
 
     return (
         <html lang="en">
@@ -48,9 +37,7 @@ export default async function RootLayout({
                 className={`${plus_jakarta_sans.className} antialiased bg-[#C2E6EC] dark:bg-[#0C1222]`}
                 style={{ margin: "0" }}
             >
-                {session?.user?.email
-                    ? protected_routes ?? redirect("/")
-                    : unprotected_routes}
+                        {session?.user?.email? protected_routes ?? redirect("/") : unprotected_routes}
                 <Toaster />
                 <SocialMediaFollowToast />
             </body>
