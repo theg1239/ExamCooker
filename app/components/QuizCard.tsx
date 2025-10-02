fba59655fc54f556786cecfa70a5d491e9246d43
theg <thegreatestdropper@gmail.com>
2025-10-02 23:36:50 +0530

    another revert

--- app/components/QuizCard.tsx
+++ app/components/QuizCard.tsx
@@ -1,44 +1,24 @@
 import React from "react";
-
+import FavouriteIcon from "/public/FavouriteIcon.svg";
+import Image from "next/image";
 interface QuizCardProps {
   courseName: string;
-  availableQuestions: number;
-  isFeatured?: boolean; 
-  onClick: () => void;
+  courseCode: string;
 }
 
-const QuizCard: React.FC<QuizCardProps> = ({
-  courseName,
-  availableQuestions,
-  isFeatured = false,
-  onClick,
-}) => {
+const QuizCard: React.FC<QuizCardProps> = ({ courseName, courseCode }) => {
   return (
-    <div
-      onClick={onClick}
-      className={`border-2 
-        ${
-          // isFeatured ? "border-red-500 dark:border-red-500" : 
-          "border-[#5FC4E7] dark:border-[#ffffff]/20 hover:border-b-[#ffffff] dark:hover:border-b-[#3BF4C7]"
-        } 
-      hover:shadow-xl px-5 py-6 w-full bg-[#5FC4E7] dark:bg-[#ffffff]/10 lg:dark:bg-[#0C1222] transition duration-200 transform hover:scale-105 max-w-96 cursor-pointer flex flex-col justify-between relative`}
-    >
-      <h3 className="text-xl font-semibold mb-4 dark:text-white">{courseName}</h3>
-      
-      <div className="flex justify-between items-center">
-        <div>
-          <span className="text-sm dark:text-[#D5D5D5]">
-            {availableQuestions} Question{availableQuestions !== 1 ? "s" : ""}
-          </span>
+    <div className="bg-[#5FC4E7] dark-[#0C1222] h-[10vh] w-[30vw] flex flex-col justify-center space-y-4 p-2 px-2">
+      <div className="flex justify-start pl-4 text-black dark:text-white">
+        {courseName}
+      </div>
+      <div className="flex flex-row justify-between">
+        <div className="flex justify-start pl-4 text-black dark:text-white">
+          {courseCode}
         </div>
-
-        {/* 
-        {isFeatured && (
-          <span className="inline-block px-2 py-1 text-xs font-semibold text-white bg-red-500 rounded">
-            Featured
-          </span>
-        )} 
-        */}
+        <div className="pr-4">
+          <Image src={FavouriteIcon} alt="favicon" />
+        </div>
       </div>
     </div>
   );
